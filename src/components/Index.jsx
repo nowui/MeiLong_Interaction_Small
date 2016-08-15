43e6f41a412270b7c175352178dcefb93f6b3818
Zhong Yong Qiang <258003787@qq.com>
2016-08-15 10:38:42 +0800

    2016-08-15 10:37

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -19,26 +19,34 @@ class Index extends Component {
   onClickMenu(index) {
     event.preventDefault()
 
-    let url = '/' + index + '/index'
+    if(index == 5) {
+      this.props.socket.emit('open', index)
+    } else if(index == -1) {
+      this.props.socket.emit('open', index)
+    } else {
+      let url = '/' + index + '/index'
 
-    this.props.socket.emit('push', url)
+      this.props.socket.emit('push', url)
 
-    self.props.router.push({
-      pathname: url,
-      query: {
+      self.props.router.push({
+        pathname: url,
+        query: {
 
-      }
-    })
+        }
+      })
+    }
   }
 
   render() {
     return (
     	<div className={styles.bg}>
+        <div className={styles.menu_home} onClick={this.onClickMenu.bind(this, -1)}></div>
         <div className={styles.menu_0} onClick={this.onClickMenu.bind(this, 0)}></div>
         <div className={styles.menu_1} onClick={this.onClickMenu.bind(this, 1)}></div>
         <div className={styles.menu_2} onClick={this.onClickMenu.bind(this, 2)}></div>
         <div className={styles.menu_3} onClick={this.onClickMenu.bind(this, 3)}></div>
         <div className={styles.menu_4} onClick={this.onClickMenu.bind(this, 4)}></div>
+        <div className={styles.menu_5} onClick={this.onClickMenu.bind(this, 5)}></div>
       </div>
     )
   }
