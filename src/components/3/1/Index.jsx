04220f58ffe37c0da1c4610f140f8789771eae51
NowUI <nowui@163.com>
2016-10-11 13:53:55 +0800

    2016-10-11 13:53

--- src/components/3/1/Index.jsx
+++ src/components/3/1/Index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import { Spin } from 'antd'
+import Helper from '../../../common/Helper'
 import styles from './Index.less'
 
 let self
@@ -10,16 +12,48 @@ class Detail extends Component {
     super(props)
 
     self = this
+
+    this.state = {
+      isLoad: false,
+      list: []
+    }
   }
 
   componentDidMount() {
+    this.load()
+  }
 
+  load = function() {
+    self.setState({
+      isLoad: true
+    })
+
+    Helper.ajax({
+      url: '/services/djlhsgkML',
+      data: {
+
+      },
+      success: function(data) {
+        self.setState({
+          list: data
+        })
+
+        if(data.length > 0) {
+          self.onClickMenu(data[0].id)
+        }
+      },
+      complete: function() {
+        self.setState({
+          isLoad: false
+        })
+      }
+    })
   }
 
   onClickMenu(index) {
     event.preventDefault()
 
-    //this.props.socket.emit('open', index)
+    this.props.socket.emit('open', index)
   }
 
   onClickBack() {
@@ -32,13 +66,37 @@ class Detail extends Component {
     self.props.router.goBack()
   }
 
+  onClickLeft() {
+    event.preventDefault()
+
+    this.props.socket.emit('up', '')
+  }
+
+  onClickRight() {
+    event.preventDefault()
+
+    this.props.socket.emit('down', '')
+  }
+
   render() {
     return (
-      <div className={styles.bg}>
-        <div className={styles.back} onClick={this.onClickBack.bind(this)}></div>
-        <div className={styles.menu_0} onClick={this.onClickMenu.bind(this, 0)}></div>
-        <div className={styles.menu_1} onClick={this.onClickMenu.bind(this, 1)}></div>
-      </div>
+      <Spin size="large" spinning={this.state.isLoad}>
+	      <div className={styles.bg}>
+	        <div className={styles.back} onClick={this.onClickBack.bind(this)}></div>
+          <div className={styles.list}>
+            {
+              this.state.list.map(function (item, index) {
+                return (
+                  <div key={index} className={styles.menu} onClick={this.onClickMenu.bind(this, item.id)}>{item.title}</div>
+                )
+              }.bind(this))
+            }
+          </div>
+	        <div className={styles.tese}></div>
+          <div className={styles.menu_0} onClick={this.onClickLeft.bind(this)}></div>
+          <div className={styles.menu_1} onClick={this.onClickRight.bind(this)}></div>
+	      </div>
+      </Spin>
     )
   }
 }
